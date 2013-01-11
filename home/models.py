8fb5db7eae19d11bc523d93f101fa8c53fcc98c3
Hoang Nguyen <hoanghw@berkeley.edu>
2013-01-11 13:12:35 -0800

    add models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Location(models.Model):
+    garage=models.CharField(max_length=30)
+    space=models.IntegerField
+
+class Pub_Transaction(models.Model):
+    method=models.CharField(max_length=30)
+    date=models.DateField
+    start=models.DateTimeField
+    end=models.DateTimeField
+    loc=models.ForeignKey(Location)
+
+class UID_Transaction(models.Model):
+    user=models.ForeignKey(User)
+    date=models.DateField
+    start=models.DateTimeField
+    end=models.DateTimeField
+    loc=models.ForeignKey(Location)
+
--- home/views.py
+++ home/views.py
@@ -1,1 +1,4 @@
 # Create your views here.
+
+def extend_parking(request):
+
