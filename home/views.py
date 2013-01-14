db0534456fff56bcf45cd2d29230c23a793bfb4b
Hoang Nguyen <hoanghw@hotmail.com>
2013-01-14 01:41:48 -0800

    check in check out

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.template import loader,Context
 from django.contrib.auth.models import User
+from home.models import UID_Transaction,Pub_Transaction,Location
 
 def extend_parking(request):
     if request.user.is_authenticated():
@@ -12,19 +13,99 @@ def extend_parking(request):
     else:
         return HttpResponseForbidden()
 
-def check_in(request):
-    if request.user.is_authenticated():
-        template=loader.get_template('home/checkin.html')
+def response(request):
+    import datetime
+    now=datetime.datetime.now()
+    message='You have checked out at '+now.ctime()+'.  The total cost of $6 will be deducted from your account.'
+    return HttpResponse(message)
 
-        return HttpResponseRedirect('home/checkin.html')
-    else:
-        return HttpResponseForbidden()
+#/?user=userName&g=garageName&s=spaceNumber&h=hours
+def user_check_in(request):
+    import datetime
+    now=datetime.datetime.now()
 
-def check_out(request):
+    if 'user' in request.GET and 'g' in request.GET and 's' in request.GET and 'h' in request.GET:
+        l=Location.objects.filter(garage=request.GET['g']).get(space=int(request.GET['s']))
+        u=User.objects.get(username=request.GET['user'])
+
+        if request.GET['h']:
+            interval=datetime.timedelta(hours=int(request.GET['h']))
+        else:
+            interval=datetime.timedelta(hours=1)
+
+        t=UID_Transaction.objects.create(user=u,
+            date=datetime.date.today(),
+            loc=l,
+            start=now,
+            end=now+interval)
+        t.save()
+
+        message="Parking info: "+unicode(l)+ " "+unicode(t)
+        return HttpResponse(message)
+    else: return HttpResponseForbidden()
+
+#/?g=garageName&s=spaceNumber&p=paymentType&h=hours
+def guest_check_in(request):
+    import datetime
+    now=datetime.datetime.now()
+
+    if 'g' in request.GET and 's' in request.GET and 'p' in request.GET and 'h' in request.GET:
+        l=Location.objects.filter(garage=request.GET['g']).get(space=int(request.GET['s']))
+
+        if request.GET['h']:
+            interval=datetime.timedelta(hours=int(request.GET['h']))
+        else:
+            interval=datetime.timedelta(hours=1)
+
+        t=Pub_Transaction.objects.create(method=request.GET['p'],
+            date=datetime.date.today(),
+            loc=l,
+            start=now,
+            end=now+interval)
+        t.save()
+
+        message="Parking info: "+unicode(l)+ " "+unicode(t)
+
+        return HttpResponse(message)
+    else: return HttpResponseForbidden()
+#/?user=userName
+def user_check_out(request):
+    import datetime
+    now=datetime.datetime.now()
+
+    if 'user' in request.GET and request.GET['user']:
+        t=UID_Transaction.objects.filter(user__username=request.GET['user']).get(end__gte=now)
+        t.end=now
+        t.save()
+
     if request.user.is_authenticated():
-        return  HttpResponseRedirect('home/checkout.html')
-    else:
-        return  HttpResponseForbidden()
+        t=UID_Transaction.objects.filter(user_id=request.user.id).get(end__gte=now)
+        t.end=now
+        t.save()
+
+    message='You have checked out at '+now.ctime()+'.  The total cost of $6 will be deducted from your account.'
+    #template=loader.get_template('home/checkout.html')
+    #context=Context({'message':message})
+    return  HttpResponse(message)
+
+
+#/?g=garageName&s=spaceNumber
+def guest_check_out(request):
+    import datetime
+    now=datetime.datetime.now()
+
+    if 'g' in request.GET and 's' in request.GET:
+        l=Location.objects.filter(garage=request.GET['g']).get(space=int(request.GET['s']))
+        t=Pub_Transaction.objects.filter(loc=l).get(end__gte=now)
+        t.end=now
+        t.save()
+
+    message='You have checked out at '+now.ctime()+'.  The total cost is $6.'
+    #template=loader.get_template('home/checkout.html')
+    #context=Context({'message':message})
+    return HttpResponse(message)
+
+
 
 def parking_status(request):
     import datetime
--- parkingsystem/urls.py
+++ parkingsystem/urls.py
@@ -11,6 +11,11 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^parking/','home.views.parking_status'),
+    url(r'^gcheckout/','home.views.guest_check_out'),
+    url(r'^ucheckout/','home.views.user_check_out'),
+    url(r'^response/','home.views.response'),
+    url(r'^ucheckin/','home.views.user_check_in'),
+    url(r'^gcheckin/','home.views.guest_check_in'),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
