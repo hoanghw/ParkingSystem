4cd01bdac2d16e35a8a28278119c999855b67b97
Hoang Nguyen <hoanghw@berkeley.edu>
2013-05-16 12:01:04 -0700

    change to one-one relationship in qtm model

--- qtmessage/models.py
+++ qtmessage/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Message(models.Model):
-    user=models.ForeignKey(User)
+    user=models.OneToOneField(User)
     widget=models.TextField()
     timetrigger=models.TextField()
     password=models.CharField(max_length=60)
